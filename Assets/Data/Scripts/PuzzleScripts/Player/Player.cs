3743261a4f86695102ffe76a5570cf8774d523bb
DryFear <voronin8945@mail.ru>
2020-02-02 13:52:01 +0300

    Add Immune Frames Fix Bugs

--- Assets/Data/Scripts/PuzzleScripts/Player/Player.cs
+++ Assets/Data/Scripts/PuzzleScripts/Player/Player.cs
@@ -8,7 +8,10 @@ namespace Puzzle
     public class Player : MonoBehaviour
     {
         private PlayerView _view;
-        
+
+        private bool _immuneFramesEnabled;
+        private float _immuneTime = 0.2f;
+
         [SerializeField]
         private int _health = 3;
         public bool[] sides = {false, false, true, true}; //It's relative to Side // True means it's stick out
@@ -17,6 +20,7 @@ namespace Puzzle
         void Start()
         {
             _view = GetComponent<PlayerView>();
+            _immuneFramesEnabled = false;
         }
         
         private void OnTriggerEnter2D(Collider2D other)
@@ -28,16 +32,26 @@ namespace Puzzle
         public void DealDamage(int damage)
         {
             GameSceneManager.Instance.ShakeCamera();
-            _health -= damage;
-            for (int i = 0; i < damage; i++)
-            {
-                GameSceneManager.Instance.InvokePlayerLosedHp(_health);
+            if (!_immuneFramesEnabled) {
+                _health -= damage;
+                for (int i = 0; i < damage; i++)
+                {
+                    GameSceneManager.Instance.InvokePlayerLosedHp(_health);
+                }
+                if (_health == 0)
+                {
+                    GameSceneManager.Instance.InvokePlayerDied();
+                }
             }
-            if (_health == 0)
-            {
-                GameSceneManager.Instance.InvokePlayerDied();
-            }
+            _immuneFramesEnabled = true;
+            Invoke( "DisableImmune", _immuneTime);
         }
+
+        private void DisableImmune()
+        {
+            _immuneFramesEnabled = false;
+        }
+        
          
         public void ChangeSides()
         {
