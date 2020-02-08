904ba66d7ff531e2fe39726bb0040e3e49807a60
DryFear <voronin8945@mail.ru>
2020-02-08 08:03:35 +0300

    New script: AttemptManager. It count attempts and show and fade text with them. In levels: added text for attempts and attach AttemptManager script for gameContainer

---
+++ Assets/Data/Scripts/PuzzleScripts/View/AttemptManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Puzzle;
+
+public class AttemptManager : MonoBehaviour
+{
+
+    private int _attemptNumper;
+
+    [SerializeField]
+    private Text _textPlaceholder;
+
+    [SerializeField]
+    private float _timeUntilStartFade = 1.0f;
+
+    [SerializeField]
+    private float _fadeTime = 1.0f;
+
+    [SerializeField]
+    private string _textOfNotification = "Attempt #";
+
+    void Start()
+    {
+        _attemptNumper = 0;
+    }
+
+    private void OnEnable()
+    {
+        GameSceneManager.ResetLevelEvent += ResetLevelEvent_Handler;
+    }
+
+    private void OnDisable()
+    {
+        GameSceneManager.ResetLevelEvent -= ResetLevelEvent_Handler;
+
+    }
+
+    void ResetLevelEvent_Handler()
+    {
+        _attemptNumper++;
+        _textPlaceholder.text = "Attempt #" + _attemptNumper;
+        StartCoroutine(FadeText());
+    }
+
+    private IEnumerator FadeText()
+    {
+        _textPlaceholder.gameObject.SetActive(true);
+        _textPlaceholder.color = new Color(_textPlaceholder.color.r, _textPlaceholder.color.g, _textPlaceholder.color.b, 1);
+        yield return new WaitForSeconds(_timeUntilStartFade);
+        while(_textPlaceholder.color.a > 0)
+        {
+            _textPlaceholder.color = new Color(_textPlaceholder.color.r, _textPlaceholder.color.g, _textPlaceholder.color.b, _textPlaceholder.color.a - (Time.deltaTime / _fadeTime));
+            yield return null;
+        }
+        _textPlaceholder.gameObject.SetActive(false);
+
+    }
+}
