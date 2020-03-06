b246b0efc0f7577664dda4f60ce40c9a18b50f00
DryFear <voronin8945@mail.ru>
2020-03-06 23:39:22 +0300

    Create Tutorial Level and NextTimelineMarker

---
+++ Assets/Data/Scripts/PuzzleScripts/Markers/NextTimelineMarker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PuzzleScripts;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class NextTimelineMarker : Marker, INotification
+{
+    [SerializeField] public TimelinePlayable nextTimeline;
+    public PropertyName id { get; }
+}
---
+++ Assets/Levels/Tutorial/Scripts/NeonPuzzle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Puzzle;
+using PuzzleScripts;
+using UnityEngine;
+
+public class NeonPuzzle : PuzzleEnemy
+{
+    public override void Instantiate(EnemyParams @params)
+    {
+        base.Instantiate(@params);
+        GetComponent<SpriteRenderer>().flipX = !@params.stickOut;
+    }
+}
