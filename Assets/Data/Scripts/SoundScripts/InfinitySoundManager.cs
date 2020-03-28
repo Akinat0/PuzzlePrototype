ed73fc72d9f41d6af75d91441de040fff8a186f8
Astrocatling <konstantdarl@gmail.com>
2020-03-28 19:31:03 +0300

    Real added InfinitySoundManager

---
+++ Assets/Data/Scripts/SoundScripts/InfinitySoundManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class InfinitySoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip theme;
+    private AudioSource currentThemeSource;
+
+    private void Start()
+    {
+        PlayTheme(theme);
+    } 
+
+    private void PlayTheme(AudioClip clip)
+    {
+        if(currentThemeSource != null) 
+            Destroy(currentThemeSource.gameObject);
+        
+        currentThemeSource = new GameObject("Theme " + clip.name).AddComponent<AudioSource>();
+        currentThemeSource.clip = clip;
+        currentThemeSource.Play();
+    }
+}
