b0c2fb0dc271c44ec10607844f2514fe7483a77d
Amir <abu.amir@mail.ru>
2020-04-08 23:52:04 +0300

    Enemy and player prefabs added

---
+++ Assets/Levels/Scripts/NeonParatyEnemy.cs
@@ -0,0 +1,12 @@
+﻿using Puzzle;
+using PuzzleScripts;
+using UnityEngine;
+
+public class NeonParatyEnemy : PuzzleEnemy
+{
+    public override void Instantiate(EnemyParams @params)
+    {
+        base.Instantiate(@params);
+        GetComponent<SkinContainer>().Skin = @params.stickOut ? 0 : 1;
+    }
+}
