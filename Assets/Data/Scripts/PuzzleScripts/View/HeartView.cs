1e09a3fe4036ee3a64451d6367002f4ef7efdbeb
Amir <abu.amir@mail.ru>
2020-04-12 20:32:45 +0300

    HeartPrefab, animation and Dissolve shader and material added

---
+++ Assets/Data/Scripts/PuzzleScripts/View/HeartView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class HeartView : MonoBehaviour
+{
+    private Animator animator;
+    private static readonly int Visible = Animator.StringToHash("Visible");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Disappear()
+    {
+        animator.SetBool(Visible, false);
+    }
+    public void Appear()
+    {
+        animator.SetBool(Visible, true);
+    }
+}
