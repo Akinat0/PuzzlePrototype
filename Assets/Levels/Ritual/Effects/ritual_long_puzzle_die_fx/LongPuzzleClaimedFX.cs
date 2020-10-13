8a452b28187adf58cadef490ec3fe4e9a82d2675
Amir <abu.amir@mail.ru>
2020-10-14 02:29:31 +0300

    Ritual long puzzle claim fx

---
+++ Assets/Levels/Ritual/Effects/ritual_long_puzzle_die_fx/LongPuzzleClaimedFX.cs
@@ -0,0 +1,11 @@
+﻿using Puzzle;
+using UnityEngine;
+
+public class LongPuzzleClaimedFX : MonoBehaviour
+{
+    void Awake()
+    {
+        if(VFXManager.Instance != null && GameSceneManager.Instance != null)
+            VFXManager.Instance.CallTapRippleEffect(GameSceneManager.Instance.Player.transform.position);        
+    }
+}
