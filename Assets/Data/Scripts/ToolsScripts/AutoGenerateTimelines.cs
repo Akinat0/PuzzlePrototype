edf7422026cf04bc545066b980054596fe6d789d
Astrocatling <konstantdarl@gmail.com>
2020-11-08 20:55:04 +0300

    Добавил автогенерацию выделенного таймлайна, при запуске игры, для удобства разработки таймлайна

---
+++ Assets/Data/Scripts/ToolsScripts/AutoGenerateTimelines.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+#if UNITY_EDITOR
+namespace Data.Scripts.Editor
+{
+    public class AutoGenerateTimelines : MonoBehaviour
+    {
+        private void Start()
+        {
+            GenearteTimelines();
+        }
+
+        private static void GenearteTimelines()
+        {
+            TimelineAsset[] assets = Selection.GetFiltered<TimelineAsset>(SelectionMode.Assets);
+            List<string> guids = new List<string>();
+            Debug.Log("AG");
+
+            foreach (TimelineAsset asset in assets)
+                guids.Add(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(asset)));
+        
+            foreach (var guid in guids)
+            {
+                TimelineAsset oldTimeline = (TimelineAsset)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid), typeof(TimelineAsset));
+
+                if (oldTimeline == null)
+                {
+                    Debug.LogError("Asset " + AssetDatabase.GUIDToAssetPath(guid) + " is not timeline");
+                    continue;
+                }
+
+                string processedPath = "Assets/Timelines/ProcessedTimelines/_" + oldTimeline.name + ".playable";
+
+                TimelineAsset newTimeline = (TimelineAsset)AssetDatabase.LoadAssetAtPath(processedPath, typeof(TimelineAsset));
+
+                if (newTimeline == null) {
+
+                    AssetDatabase.CopyAsset(AssetDatabase.GUIDToAssetPath(guid), processedPath);
+                    newTimeline = (TimelineAsset)AssetDatabase.LoadAssetAtPath(processedPath, typeof(TimelineAsset));
+                }
+            
+                TimelineProcessor.GenerateNewTimeline(oldTimeline, newTimeline);
+            } 
+        }
+    }
+}
+
+#endif
