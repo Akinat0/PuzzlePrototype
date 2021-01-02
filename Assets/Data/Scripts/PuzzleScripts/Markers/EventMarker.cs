a4d4e940a1663b4487719f66e483e67b4217b772
Amir <abu.amir@mail.ru>
2021-01-02 22:06:13 +0300

    Add event marker style icon. Support difficulty levels for Ritual.

--- Assets/Data/Scripts/PuzzleScripts/Markers/EventMarker.cs
+++ Assets/Data/Scripts/PuzzleScripts/Markers/EventMarker.cs
@@ -2,6 +2,7 @@
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
+[CustomStyle("EventMarker")]
 public class EventMarker : Marker, INotification
 {
     [SerializeField] private string eventData;
