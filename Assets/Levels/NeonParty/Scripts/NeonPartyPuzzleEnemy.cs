cd49a51d3d1e155b4b23354c171fa90dc182dac4
Amir <abu.amir@mail.ru>
2021-02-11 20:59:37 +0300

    Fix neon

--- Assets/Levels/NeonParty/Scripts/NeonPartyPuzzleEnemy.cs
+++ Assets/Levels/NeonParty/Scripts/NeonPartyPuzzleEnemy.cs
@@ -21,7 +21,7 @@ public class NeonPartyPuzzleEnemy : PuzzleEnemy
     public override void Instantiate(EnemyParams @params)
     {
         base.Instantiate(@params);
-        SkinContainer.Skin = @params.stickOut ? 0 : 1;
+        SkinContainer.Skin = @params.stickOut ? 1 : 0;
         
         if(@params.side == Side.Right)
             transform.Rotate(new Vector3(180, 0, 0));
